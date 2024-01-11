028c8455695edd1d8b397f8a7a1d34fb18dea4d3
Rafael Epplée <hello@rafa.ee>
2024-01-11 13:20:50 +0100

    Add starship

--- install.sh
+++ install.sh
@@ -3,8 +3,12 @@
 set -euxo pipefail
 
 cp .zshrc .zshenv ~
+echo "export PATH=\$PATH:$(dirname "$0")/bin" >> ~/.zshenv
 touch ~/.zshrc.local ~/.zshenv.local
 
 if [[ ! "$SHELL" =~ "zsh$" ]]; then
     chsh -s /usr/bin/zsh
 fi
+
+mkdir -p ~/.config/starship
+cp starship.toml ~/.config/starship
